987e061770dde23938e9be1aaad5d44ed924f7ff
GermanAizek <GermanAizek@yandex.ru>
2021-06-29 15:10:23 +0300

    Cloned from https://github.com/oneiric/vld.git

---
+++ setup/version.h
@@ -0,0 +1,9 @@
+
+#define VLDVERSION          L"2.6.0"
+#define VERSION_NUMBER		2,6,0,0
+#define VERSION_STRING		"2.6.0.0"
+#define VERSION_COPYRIGHT	"Copyright (C) 2005-2020"
+
+#ifndef __FILE__
+!define VLD_VERSION "2.6.0"	// NSIS Script
+#endif
